8b52bd64fe0393955e21eb12f026e83deb544eb4
Viswajith <120708489+Viswajiz@users.noreply.github.com>
2023-08-21 19:30:44 +0530

    Add files via upload

---
+++ Array_input.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Array_input {
+  /*  static int fun(int a){
+        for (int i=0;i<5;i++){
+            if (a[i] %2 !=0){
+                System.out.println("Odd"+a[i]);
+
+            }}
+*/
+    }
+    /*public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of array: ");
+        int n = input.nextInt();
+
+        int[] a = new int[n];
+
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the element: ");
+            a[i] = input.nextInt();
+        }System.out.println(Arrays.toString(a));
+
+
+
+
+
+
+        }
+
+    }
+*/
